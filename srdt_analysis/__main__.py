386853bb7876490bc628f386006cca94bc6afb2b
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-11-14 10:28:51 +0100

    fix: remote

--- srdt_analysis/__main__.py
+++ srdt_analysis/__main__.py
@@ -1,11 +1,14 @@
 from dotenv import load_dotenv
-from .exploit_data import exploit_data
+from srdt_analysis.llm import get_llm
+from srdt_analysis.exploit_data import exploit_data
+
 
 load_dotenv()
 
 
 def main():
-    exploit_data()
+    # exploit_data()
+    get_llm()
 
 
 if __name__ == "__main__":
--- srdt_analysis/llm.py
+++ srdt_analysis/llm.py
@@ -1,20 +1,40 @@
-import ollama
+import httpx
+import os
 
 
 def get_extended_data(message: str, is_summary: bool) -> str:
-    summary_prompt = "Tu es un chatbot expert en droit du travail français. Lis le texte donné et rédige un résumé clair, précis et concis, limité à 4096 tokens maximum. Dans ce résumé, fais ressortir les points clés suivants : Sujets principaux : identifie les droits et obligations des employeurs et des salariés, les conditions de travail, les procédures, etc. Langage clair : simplifie le langage juridique tout en restant précis pour éviter toute confusion. Organisation logique : commence par les informations principales, puis détaille les exceptions ou points secondaires s'ils existent. Neutralité : garde un ton factuel, sans jugement ou interprétation subjective. Longueur : si le texte est long, privilégie les informations essentielles pour respecter la limite de 4096 tokens."
-    keyword_prompt = "Tu es un chatbot expert en droit du travail français. Ta seule mission est d’extraire une liste de mots-clés à partir du texte fourni. Objectif : Les mots-clés doivent refléter les idées et thèmes principaux pour faciliter la compréhension et la recherche du contenu du texte. Sélection : Extrait uniquement les termes essentiels, comme les droits et devoirs des employeurs et des salariés, les conditions de travail, les procédures, les sanctions, etc. Non-redondance : Évite les répétitions ; chaque mot-clé doit apparaître une seule fois. Clarté et simplicité : Assure-toi que chaque mot-clé est compréhensible et pertinent. Format attendu pour la liste de mots-clés : une liste simple et directe, sans organisation par thèmes, comme dans cet exemple : code du travail, article 12, congés payés, heures supplémentaires, licenciement économique"
-    response = ollama.chat(
-        model="mistral-nemo",
-        messages=[
-            {
-                "role": "system",
-                "content": summary_prompt if is_summary else keyword_prompt,
+    summary_prompt = "Tu es un chatbot expert en droit du travail français. Lis le texte donné et rédige un résumé clair, précis et concis, limité à 4096 tokens maximum. Dans ce résumé, fais ressortir les points clés suivants : Sujets principaux : identifie les droits et obligations des employeurs et des salariés, les conditions de travail, les procédures, etc. Langage clair : simplifie le langage juridique tout en restant précis pour éviter toute confusion. Organisation logique : commence par les informations principales, puis détaille les exceptions ou points secondaires s'ils existent. Neutralité : garde un ton factuel, sans jugement ou interprétation subjective. Longueur : si le texte est long, privilégie les informations essentielles pour respecter la limite de 4096 tokens. Ta réponse doit obligatoirement être en français."
+    keyword_prompt = "Tu es un chatbot expert en droit du travail français. Ta seule mission est d’extraire une liste de mots-clés à partir du texte fourni. Objectif : Les mots-clés doivent refléter les idées et thèmes principaux pour faciliter la compréhension et la recherche du contenu du texte. Sélection : Extrait uniquement les termes essentiels, comme les droits et devoirs des employeurs et des salariés, les conditions de travail, les procédures, les sanctions, etc. Non-redondance : Évite les répétitions ; chaque mot-clé doit apparaître une seule fois. Clarté et simplicité : Assure-toi que chaque mot-clé est compréhensible et pertinent. Format attendu pour la liste de mots-clés : une liste simple et directe, sans organisation par thèmes, comme dans cet exemple : code du travail, article 12, congés payés, heures supplémentaires, licenciement économique. Ta réponse doit obligatoirement être en français."
+
+    api_key = os.getenv("ALBERT_API_KEY")
+    if not api_key:
+        raise ValueError("API key for Albert is not set")
+
+    try:
+        response = httpx.post(
+            "https://albert.api.etalab.gouv.fr/v1/embeddings",
+            headers={"Authorization": f"Bearer {api_key}"},
+            json={
+                "messages": [
+                    {
+                        "role": "system",
+                        "content": summary_prompt if is_summary else keyword_prompt,
+                    },
+                    {
+                        "role": "user",
+                        "content": message,
+                    },
+                ],
+                "model": "meta-llama/Meta-Llama-3.1-70B-Instruct",
             },
-            {
-                "role": "user",
-                "content": message,
-            },
-        ],
-    )
-    return response["message"]["content"]
+        )
+        response.raise_for_status()
+        chat_response = response.json()["choices"][0]["message"]["content"]
+    except httpx.HTTPStatusError as e:
+        raise RuntimeError(
+            f"Request failed: {e.response.status_code} - {e.response.text}"
+        )
+    except Exception as e:
+        raise RuntimeError(f"An error occurred: {str(e)}")
+
+    return chat_response
