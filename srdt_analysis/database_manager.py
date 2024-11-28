20ab6f6b10f13f6bf72668f4406eb938899a9d53
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-11-28 10:36:59 +0100

    feat(db): refacto

--- srdt_analysis/database_manager.py
+++ srdt_analysis/database_manager.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
-from typing import Tuple
+from contextlib import asynccontextmanager
+from typing import Optional, Tuple
 
 import asyncpg
 
@@ -9,10 +10,10 @@ from srdt_analysis.models import Document, DocumentsList
 
 class DatabaseManager:
     def __init__(self):
-        self.conn
+        self.pool: Optional[asyncpg.Pool] = None
 
     async def connect(self):
-        self.conn = await asyncpg.connect(
+        self.pool = await asyncpg.create_pool(
             user=os.getenv("POSTGRES_USER"),
             password=os.getenv("POSTGRES_PASSWORD"),
             database=os.getenv("POSTGRES_DATABASE_NAME"),
@@ -20,69 +21,66 @@ class DatabaseManager:
         )
 
     async def close(self):
-        if self.conn:
-            await self.conn.close()
+        if self.pool:
+            await self.pool.close()
+
+    @asynccontextmanager
+    async def get_connection(self):
+        if not self.pool:
+            await self.connect()
+        if self.pool is None:
+            raise ValueError("Pool is not initialized")
+        async with self.pool.acquire() as conn:
+            yield conn
+
+    async def fetch_documents_by_source(self, source: str) -> DocumentsList:
+        async with self.get_connection() as conn:
+            result = await conn.fetch(
+                "SELECT * from public.documents WHERE source = $1", source
+            )
+            return [Document.from_record(r) for r in result]
 
     async def fetch_articles_code_du_travail(self) -> DocumentsList:
-        results = await self.conn.fetch(
-            "SELECT * from public.documents WHERE source = 'code_du_travail'"
-        )
-        return [Document.from_record(r) for r in results]
+        return await self.fetch_documents_by_source("code_du_travail")
 
     async def fetch_fiches_mt(self) -> DocumentsList:
-        result = await self.conn.fetch(
-            "SELECT * from public.documents WHERE source = 'page_fiche_ministere_travail'"
-        )
-        return [Document.from_record(r) for r in result]
+        return await self.fetch_documents_by_source("page_fiche_ministere_travail")
 
     async def fetch_fiches_sp(self) -> DocumentsList:
-        result = await self.conn.fetch(
-            "SELECT * from public.documents WHERE source = 'fiches_service_public'"
-        )
-        return [Document.from_record(r) for r in result]
+        return await self.fetch_documents_by_source("fiches_service_public")
 
     async def fetch_page_infos(self) -> DocumentsList:
-        result = await self.conn.fetch(
-            "SELECT * from public.documents WHERE source = 'information'"
-        )
-        return [Document.from_record(r) for r in result]
+        return await self.fetch_documents_by_source("information")
 
     async def fetch_page_contribs(self) -> DocumentsList:
-        result = await self.conn.fetch(
-            "SELECT * from public.documents WHERE source = 'contributions'"
-        )
-        return [Document.from_record(r) for r in result]
+        return await self.fetch_documents_by_source("contributions")
 
     async def fetch_all(
         self,
     ) -> Tuple[
-        DocumentsList,
-        DocumentsList,
-        DocumentsList,
-        DocumentsList,
-        DocumentsList,
+        DocumentsList, DocumentsList, DocumentsList, DocumentsList, DocumentsList
     ]:
-        await self.connect()
-
-        result1 = await self.fetch_articles_code_du_travail()
-        result2 = await self.fetch_fiches_mt()
-        result3 = await self.fetch_fiches_sp()
-        result4 = await self.fetch_page_infos()
-        result5 = await self.fetch_page_contribs()
-
-        await self.close()
-
-        return (result1, result2, result3, result4, result5)
+        try:
+            results = await asyncio.gather(
+                self.fetch_articles_code_du_travail(),
+                self.fetch_fiches_mt(),
+                self.fetch_fiches_sp(),
+                self.fetch_page_infos(),
+                self.fetch_page_contribs(),
+            )
+            return (
+                results[0],
+                results[1],
+                results[2],
+                results[3],
+                results[4],
+            )
+        finally:
+            await self.close()
 
 
 def get_data() -> (
-    Tuple[
-        DocumentsList,
-        DocumentsList,
-        DocumentsList,
-        DocumentsList,
-        DocumentsList,
-    ]
+    Tuple[DocumentsList, DocumentsList, DocumentsList, DocumentsList, DocumentsList]
 ):
     db = DatabaseManager()
     return asyncio.run(db.fetch_all())
