f3afeb78dbeef18cf67242240269ba8010569395
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-11-29 13:47:48 +0100

    fix: code

--- srdt_analysis/__main__.py
+++ srdt_analysis/__main__.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from srdt_analysis.collections import Collections
 from srdt_analysis.data_exploiter import PageInfosExploiter
 from srdt_analysis.database_manager import get_data
+from srdt_analysis.llm_processor import LLMProcessor
 from srdt_analysis.mapper import Mapper
 
 load_dotenv()
@@ -20,14 +21,19 @@ def main():
     mapper = Mapper()
     data_to_send_to_llm = mapper.get_original_docs(rag_response, data)
 
-    print(data_to_send_to_llm)
-
-    # llm_processor = LLMProcessor()
+    llm_processor = LLMProcessor()
     # res = llm_processor.get_answer(
-    #     "Combien de jours de congé payé par mois de travail effectif ?", rag_response
+    #     "Combien de jours de congé payé par mois de travail effectif ?",
+    #     data_to_send_to_llm,
     # )
 
     # print(res)
+
+    for token in llm_processor.get_answer_stream(
+        "Combien de jours de congé payé par mois de travail effectif ?",
+        data_to_send_to_llm,
+    ):
+        print(token, end="", flush=True)
 
 
 if __name__ == "__main__":
