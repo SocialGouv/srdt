1874f37fbaebbb730f994bdff9b6967b3f2218d7
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-12-18 17:58:57 +0100

    fix(retours): utilisation du modèle au niveau de la requête sql

--- srdt_analysis/database_manager.py
+++ srdt_analysis/database_manager.py
@@ -33,12 +33,30 @@ class DatabaseManager:
         async with self.pool.acquire() as conn:
             yield conn
 
-    async def fetch_documents_by_source(self, source: str) -> DocumentsList:
+    async def fetch_documents_by_source(self, source: CollectionName) -> DocumentsList:
         async with self.get_connection() as conn:
-            result = await conn.fetch(
-                "SELECT * from public.documents WHERE source = $1 AND is_published = true AND is_available = true",
-                source,
-            )
+            if source == "contributions":
+                result = await conn.fetch(
+                    """
+                    SELECT * from public.documents 
+                    WHERE source = $1 
+                    AND is_published = true 
+                    AND is_available = true 
+                    AND document->>'content' IS NOT NULL 
+                    AND document->>'idcc' = '0000'
+                    """,
+                    source,
+                )
+            else:
+                result = await conn.fetch(
+                    """
+                    SELECT * from public.documents 
+                    WHERE source = $1 
+                    AND is_published = true 
+                    AND is_available = true
+                    """,
+                    source,
+                )
             return [Document.from_record(r) for r in result]
 
     async def fetch_sources(
