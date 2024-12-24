ee03a618c6bc3008de7af7c03a5648d13a651949
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-12-24 02:01:53 +0100

    feat(tokenizer): ajout du tokenizer pour calculer le nombre de token

---
+++ srdt_analysis/tokenizer.py
@@ -0,0 +1,18 @@
+import os
+
+from transformers import AutoTokenizer
+
+from srdt_analysis.constants import LLM_MODEL
+
+
+class Tokenizer:
+    def __init__(self, hf_token: str | None = None):
+        token = hf_token or os.getenv("HF_TOKEN")
+        if not token:
+            raise ValueError("HF_TOKEN not provided or found in environment")
+
+        self._tokenizer = AutoTokenizer.from_pretrained(LLM_MODEL, token=token)
+
+    def get_nb_tokens(self, text: str) -> int:
+        tokens = self._tokenizer.encode(text)
+        return len(tokens)
