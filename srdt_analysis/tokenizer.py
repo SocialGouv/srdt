b1082b805db179f91827ec8f847b9876c3ccae08
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-12-24 02:20:09 +0100

    fix(env): changement de nom pour specifier que c est un  api token

--- srdt_analysis/tokenizer.py
+++ srdt_analysis/tokenizer.py
@@ -7,9 +7,9 @@ from srdt_analysis.constants import LLM_MODEL
 
 class Tokenizer:
     def __init__(self, hf_token: str | None = None):
-        token = hf_token or os.getenv("HF_TOKEN")
+        token = hf_token or os.getenv("HF_API_TOKEN")
         if not token:
-            raise ValueError("HF_TOKEN not provided or found in environment")
+            raise ValueError("HF_API_TOKEN not provided or found in environment")
 
         self._tokenizer = AutoTokenizer.from_pretrained(LLM_MODEL, token=token)
 
