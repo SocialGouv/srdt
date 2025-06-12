cf9fe9363b823ab2b369e9c8294e55d5b42492d5
Raphaël Huchet <raphaelht@gmail.com>
2025-06-12 12:57:41 +0200

    chore: sentry sur la partie web (#141)
    * chore: sentry sur la partie web
    
    * chore: up
    
    * chore: up
    
    * chore: up
    
    * chore: up
    
    * chore: up
    
    * chore: up

---
+++ web/sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ac178a567d327d49a3b680fb482a4b33@sentry2.fabrique.social.gouv.fr/46",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
