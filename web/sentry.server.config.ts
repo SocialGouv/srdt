bf795e28117d2b109d2c4b1678f81565572f0fc2
Raphaël Huchet <raph@selego.co>
2025-06-12 12:18:25 +0200

    chore: sentry sur la partie web

---
+++ web/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ac178a567d327d49a3b680fb482a4b33@sentry2.fabrique.social.gouv.fr/46",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
