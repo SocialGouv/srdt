e1ab3c0a6587ee140ed9b3048dbb5f370f2813b7
Raphaël Huchet <raphaelht@gmail.com>
2025-07-08 10:47:57 +0200

    Update api/srdt_analysis/api/main.py
    Co-authored-by: Revu <dnum.fabrique@sg.social.gouv.fr>

--- api/srdt_analysis/api/main.py
+++ api/srdt_analysis/api/main.py
@@ -44,9 +44,9 @@ if api_host not in ["localhost", "127.0.0.1"]:
         # of transactions for tracing.
         traces_sample_rate=1.0,
     )
-    print("Sentry initialized for production environment")
+    logger.info("Sentry initialized for production environment")
 else:
-    print("Sentry disabled for local development")
+    logger.info("Sentry disabled for local development")
 
 app = FastAPI()
 api_key_header = APIKeyHeader(name="Authorization", auto_error=True)
